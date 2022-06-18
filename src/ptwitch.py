1e0cf574693abde54c9f3a7f7498669b8c94ff33
Richard Tatum <rtatum@pm.me>
2022-06-18 21:28:45 +0100

    Add ability to see and stream one of the top 20 streams

--- src/ptwitch.py
+++ src/ptwitch.py
@@ -6,9 +6,18 @@ from credentials import Credentials
 player = mpv.MPV(ytdl=True, hwdec="auto")
 credentials = Credentials()
 
-# request = requests.get("https://api.twitch.tv/helix/streams?first=20", headers=credentials.headers)
-# top_streams = request.json()["data"]
+request = requests.get("https://api.twitch.tv/helix/streams?first=20", headers=credentials.headers)
+top_streams = request.json()["data"]
 
-# streams = [(index, stream["user_login"]) for index, stream in enumerate(top_streams)]
+streams = dict([(index, stream["user_login"]) for index, stream in enumerate(top_streams)])
 
-# print(streams)
+print("Top 20 Twitch Streams")
+for index in streams:
+    print(f"{index+1}: {streams[index]}")
+
+selected = int(input("Select stream to play: "))
+
+streamer = streams[selected-1]
+
+player.play(f"https://twitch.tv/{streamer}")
+player.wait_for_playback()
